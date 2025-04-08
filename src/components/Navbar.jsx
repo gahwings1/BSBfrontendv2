cf140c662d2088117113a8b4012a833e7d9e008c
George Hoffmann <143972351+gahwings1@users.noreply.github.com>
2025-04-07 21:56:55 -0400

    Working CSS Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import { Navbar, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 export const Navbar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, seScrolled] = useState(false);
@@ -25,7 +30,7 @@ export const Navbar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={} alt="Logo" />
+                    <img src={logo} alt="Logo" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
@@ -38,9 +43,9 @@ export const Navbar = () => {
                     </Nav>
                         <span className="navbar-text">
                             <div className="social-icon">
-                                <a href="#"><img src={} alt="" /></a>
-                                <a href="#"><img src={} alt="" /></a>
-                                <a href="#"><img src={} alt="" /></a>
+                                <a href="#"><img src={navIcon1} alt="" /></a>
+                                <a href="#"><img src={navIcon2} alt="" /></a>
+                                <a href="#"><img src={navIcon3} alt="" /></a>
                             </div>
                             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                         </span>
